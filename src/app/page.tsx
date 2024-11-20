6491c6b600106f499ec229b0edf798eea49ba363
Alexandre Augustus Borges <alexandreaugustusborges@gmail.com>
2024-11-20 11:23:18 -0300

    Inicio Projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Alexandre Augustus :: Portifólio',
+}
+
+export default function Home() {
+  return (
+    <>
+      <h1>Pagina Principal</h1>
+    </>
+  );
+}
